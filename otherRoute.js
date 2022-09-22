e6e5af0c6bb9a56b4167ae228093278eae6ff6da
Iván L. Murillo <diskman199@gmail.com>
2022-09-21 21:02:57 -0500

    Send to specific sockets

--- otherRoute.js
+++ otherRoute.js
@@ -1,15 +1,28 @@
-const { Socket } = require('./sockets');
+const { Socket, getClientsWatchingId } = require('./sockets');
 
 module.exports = (app) => {
     app.route('/testing')
     .get(
         (req, res) => {
             const message = req.query.message || 'Hola mundo';
+            const id = req.query.id || null;
 
             console.log(req.query);
-            Socket.emit('test', {
-                message: message
-            });
+            
+            if(id){
+                console.log('Searching for watchers...')
+                const watchers = getClientsWatchingId(id);
+
+                console.log(watchers);
+                watchers.forEach(w => {
+                    Socket.emitSpecific(w.SocketId, 'test', { message: `Message to ${w.id} (${w.SocketId}) connected on ${w.connectedSince}` });
+                });
+
+            }else{
+                Socket.emit('test', {
+                    message: message
+                });
+            }
 
             console.log('request');
             res.json({
--- sockets.js
+++ sockets.js
@@ -2,24 +2,29 @@ const socket = require('socket.io');
 let io;
 
 const Socket = {
-    emit: function (event, data) {
+    emit: (event, data) => {
         console.log(event, data);
         io.sockets.emit(event, data);
+    },
+    emitSpecific: (client, event, data) => {
+        console.log(client, event, data);
+        io.to(client).emit(event, data);
     }
 };
 
 
+let clients = {};
 exports.io = io;
 exports.Socket = Socket;
 
 exports.attach = (app) => {
     io = socket(app);
 
-    const clients = {};
-
     io.on("connection", (socket) => {
         console.log("User connected - " + socket.id);
         clients[socket.id] = {
+            // socket: socket,
+            SocketId: socket.id,
             connectedSince: new Date()
         }
         console.log(clients);
@@ -27,7 +32,26 @@ exports.attach = (app) => {
         socket.on('hour', data => {
             console.log('la hora es: ');
             console.log(data);
-        })
-        
+        });
+
+        socket.on('id', id => {
+            clients[socket.id]['id'] = id;
+            console.log('ID RECEIVED');
+            console.log(clients[socket.id]);
+        });
+
     });
+}
+
+exports.getClientsWatchingId = (id) => {
+    let watchers = [];
+    Object.keys(clients).forEach(key => {
+        console.log(clients[key]);
+        console.log(clients[key]['id'], id);
+        if(clients[key]['id'] == id){
+            watchers.push(clients[key]);
+        }
+    });
+
+    return watchers;
 }
